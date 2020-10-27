dcd9b734532b888e259c317e81a836fc8fc0e293
Annel Cota <acch1010@cs.unm.edu>
2020-10-27 14:39:42 -0600

    added more to typing game and finished comments for scores

--- src/Scores.java
+++ src/Scores.java
@@ -1,8 +1,17 @@
+/**
+ * @author Annel Cota
+ *
+ * This Scores class keeps the scores of the games
+ * stored in arrays. It updates the scores of the games
+ * as they finish, gets the high score of each game, and
+ * updates the scores in the scores section of the home
+ * screen.
+ */
+
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
-
 import java.util.ArrayList;
 import java.util.Collections;
 
@@ -33,6 +42,10 @@ public class Scores {
     public ArrayList<Integer> verbalMemory = new ArrayList<>();
     public ArrayList<Integer> visualMemory = new ArrayList<>();
 
+    /**
+     * Gets all the score, if any, from previous games, from Main
+     * and updates labels.
+     */
     public void initialize() {
         Scores scores = Main.getScores();
         updateLabel("Aim Trainer:", aimTrainerScores, scores.getAimTrainer());
@@ -45,15 +58,60 @@ public class Scores {
         updateLabel("Visual Memory:", visualMemoryScores, scores.getVisualMemory());
     }
 
+    /**
+     * Gets Aim Trainer scores array
+     * @return aim trainer score array
+     */
     public ArrayList getAimTrainer() { return aimTrainer; }
+
+    /**
+     * Gets Chimp Test scores array
+     * @return chimp test scores array
+     */
     public ArrayList getChimpTest() { return chimpTest; }
+
+    /**
+     * Gets Match Pairs scores array
+     * @return match pairs scores array
+     */
     public ArrayList getMatchPairs() { return matchPairs; }
+
+    /**
+     * Gets Number Memory scores array
+     * @return number memory scores array
+     */
     public ArrayList getNumberMemory() { return numberMemory; }
+
+    /**
+     * Gets Reaction Time scores array
+     * @return reaction time scores array
+     */
     public ArrayList getReactionTime() { return reactionTime; }
+
+    /**
+     * Gets Typing scores array
+     * @return typing scores array
+     */
     public ArrayList getTyping() { return typing; }
+
+    /**
+     * Gets Verbal Memory scores array
+     * @return verbal memory scores array
+     */
     public ArrayList getVerbalMemory() { return verbalMemory; }
+
+    /**
+     * Gets Visual Memory scores array
+     * @return visual memory scores array
+     */
     public ArrayList getVisualMemory() { return visualMemory; }
 
+    /**
+     * Updates the label specified.
+     * @param name : the name to add in the label
+     * @param label : which label is being edited
+     * @param list : which scores array is being used
+     */
     public void updateLabel(String name, Label label, ArrayList list) {
         StringBuilder string = new StringBuilder();
         for(int i = 0; i < list.size(); i++) {
@@ -62,9 +120,18 @@ public class Scores {
         label.setText(name + string);
     }
 
+    /**
+     * Adds score to Aim Trainer array.
+     * @param totalTime : time score from aim trainer game
+     */
     public void addAimTrainerScore(long totalTime) {
         aimTrainer.add(totalTime);
     }
+
+    /**
+     * Gets the min score from the aim trainer array if its not empty.
+     * @return min score from aim trainer game
+     */
     public long getAimTrainerHighScore() {
         if(aimTrainer.isEmpty()) {
             return 0;
@@ -73,7 +140,16 @@ public class Scores {
         }
     }
 
+    /**
+     * Adds score to Chimp Test array.
+     * @param numberOfSquares : score from chimp test game
+     */
     public void addChimpTestScore(int numberOfSquares) { chimpTest.add(numberOfSquares); }
+
+    /**
+     * Gets the max score from the chimp test array if its not empty.
+     * @return max score from the chimp test game
+     */
     public int getChimpTestScore() {
         if(chimpTest.isEmpty()) {
             return 0;
@@ -82,7 +158,16 @@ public class Scores {
         }
     }
 
+    /**
+     * Adds score to Match Pairs array.
+     * @param secValue : time score from match pairs game
+     */
     public void addMatchPairsScore(long secValue) { matchPairs.add(secValue); }
+
+    /**
+     * Gets the min score from the match pairs array if its not empty.
+     * @return min score from the match pairs game
+     */
     public long getMatchPairsScore() {
         if(matchPairs.isEmpty()) {
             return 0;
@@ -91,7 +176,16 @@ public class Scores {
         }
     }
 
+    /**
+     * Adds score to Number Memory array.
+     * @param level : score from number memory game
+     */
     public void addNumberMemoryScore(int level) { numberMemory.add(level); }
+
+    /**
+     * Gets the max score from the number memory array if its not empty.
+     * @return max score from the number memory game
+     */
     public int getNumberMemoryScore() {
         if(numberMemory.isEmpty()) {
             return 0;
@@ -100,7 +194,16 @@ public class Scores {
         }
     }
 
+    /**
+     * Adds score to Reaction Time array.
+     * @param milliValue : time score from reaction time game
+     */
     public void addReactionTimeScore(long milliValue) { reactionTime.add(milliValue); }
+
+    /**
+     * Gets the min score from the reaction time array if its not empty.
+     * @return min score from reaction time game
+     */
     public long getReactionTmeScore() {
         if(reactionTime.isEmpty()) {
             return 0;
@@ -109,16 +212,34 @@ public class Scores {
         }
     }
 
-    public void addTypingScore() { }
+    /**
+     * Adds score to Typing array.
+     * @param wpm : wpm score from typing game
+     */
+    public void addTypingScore(int wpm) { typing.add(wpm); }
+
+    /**
+     * Gets the min score from the typing array if its not empty.
+     * @return min score from typing game
+     */
     public int getTypingScore() {
         if(typing.isEmpty()) {
             return 0;
         } else {
-            return Collections.max(typing);
+            return Collections.min(typing);
         }
     }
 
+    /**
+     * Adds score to Verbal Memory array.
+     * @param score : score from verbal memory game
+     */
     public void addVerbalMemoryScore(int score) { verbalMemory.add(score); }
+
+    /**
+     * Gets the max score from the verbal memory array if its not empty.
+     * @return max score from verbal memory game
+     */
     public int getVerbalMemoryScore() {
         if(verbalMemory.isEmpty()) {
             return 0;
@@ -127,7 +248,16 @@ public class Scores {
         }
     }
 
+    /**
+     * Adds score to Visual Memory array.
+     * @param level : score from visual memory game
+     */
     public void addVisualMemoryScore(int level) { visualMemory.add(level); }
+
+    /**
+     * Gets the max score from visual memory array if its not empty.
+     * @return max score from visual memory
+     */
     public int getVisualMemoryScore() {
         if(visualMemory.isEmpty()) {
             return 0;
--- src/Typing.java
+++ src/Typing.java
@@ -1,22 +1,36 @@
 /**
  * @author Annel Cota
  *
- *
+ * This Typing game class gives the player a prompt
+ * that they have to copy by typing in the text
+ * area below the prompt given. Once they're done,
+ * their wpm will be calculated.
  */
 
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.Pane;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class Typing {
     private Scores scores;
     private Scene scene;
+    private int i = 0;
+    private int characters = 0;
+    private long startTime;
+    private String userText = "";
     @FXML
     private TextArea prompt;
     @FXML
     private TextArea userInput;
+    @FXML
+    private Label highScoreLabel;
+    @FXML
+    private Label wpmLabel;
+    private String currentPrompt;
     private String prompt1 = "Water. Earth. Fire. Air. " +
             "Long ago, the four nations lived together in harmony. " +
             "Then everything changed when the Fire Nation attacked. " +
@@ -43,13 +57,50 @@ public class Typing {
             "it a character of its own.";
 
     /**
-     *
+     * When the start button is clicked, the high score label is
+     * updated if there is one and the create prompt is selected.
      */
     public void startGame() {
         //gets scores from Main
         scores = Main.getScores();
         //if there is a high score for this game, updates high score label
+        int highScore = scores.getTypingScore();
+        if(highScore != 0) { highScoreLabel.setText("High Score: " + highScore); }
+        userInput.setDisable(false);
         createPrompt();
+        startTime = System.nanoTime();
+    }
+
+    /**
+     * Once the user finishes typing and they click on the
+     * finish button, this method is called. The total time
+     * it took them is calculated and then used to calculate
+     * the wpm.
+     */
+    public void finishTyping() {
+        //finishes timer and calculates total time
+        long finishTime = System.nanoTime();
+        long totalTimeNano = finishTime - startTime;
+        long secValue = TimeUnit.NANOSECONDS.toSeconds(totalTimeNano);
+        int seconds = (int) secValue / 60;
+        //calculates words per minute
+        int wpm = (characters / 5) / seconds;
+        //add wpm to scores class
+        scores.addTypingScore(wpm);
+        wpmLabel.setText("Your speed: " + wpm + " wpm");
+    }
+
+    @FXML
+    private void checkMatch() {
+        userText = userInput.getText();
+        if(userText.charAt(i) == currentPrompt.charAt(i)) {
+            System.out.println("match");
+        } else {
+            System.out.println("no match");
+        }
+        System.out.println("current prompt: " + currentPrompt.charAt(i));
+        System.out.println("use string " + userText.charAt(i));
+        i++;
     }
 
     /**
@@ -61,10 +112,16 @@ public class Typing {
         int n = rand.nextInt(3) + 1;
         if(n == 1) {
             prompt.setText(prompt1);
+            currentPrompt = prompt1;
+            characters = 625;
         } else if(n == 2) {
             prompt.setText(prompt2);
+            currentPrompt = prompt2;
+            characters = 519;
         } else {
             prompt.setText(prompt3);
+            currentPrompt = prompt3;
+            characters = 605;
         }
     }
 
